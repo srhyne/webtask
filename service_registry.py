12acac240e0cc20b7c2f5f500b0d0aab7db28e38
Stephen Rhyne <stephen@stephenrhyne.com>
2018-03-30 19:50:53 -0500

    initial commit

---
+++ service_registry.py
@@ -0,0 +1,26 @@
+import socket
+import sys
+import os
+import time
+import random
+
+def report():
+  ns = sys.argv[1];
+  host = socket.gethostname();
+  ip = socket.gethostbyname(host)
+  url = 'https://wt-983c23213f4e911f6f4b0a243568b6d5-0.run.webtask.io/service-registry'
+
+  url += "?ns=%s&host=%s&ip=%s" % (ns, host, ip)
+
+  print(url);
+  print("\n");
+
+  os.system("curl %s" % (url) )
+  print("\n")
+
+  return
+
+while True:
+  report()
+  sleep_interval = random.uniform(0, 15)
+  time.sleep(sleep_interval)
